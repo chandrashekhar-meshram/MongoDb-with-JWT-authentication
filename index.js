186a5c07be3300d3a3a5f3b1d0c9ac4783030465
CHANDRASHEKHAR MESHRAM <107882415+chandrashekhar-meshram@users.noreply.github.com>
2023-03-02 14:04:36 +0000

    token verified

--- index.js
+++ index.js
@@ -15,12 +15,39 @@ app.post('/login', (req, resp) => {
     username: 'anil',
     email: 'abc@test.com'
   }
-  jwt.sign({user}, secretKey, {expiresIn: '300s'},( err, token) => {
+  jwt.sign({user}, secretKey, {expiresIn: '900s'},( err, token) => {
     resp.json({
       token
     })
   });
 })
+
+app.post('/profile', verifyToken, (req, resp) => {
+  jwt.verify(req.token, secretKey, (err, authData) => {
+    if(err){
+      resp.send({result: "invalid token"})
+    }else{
+      resp.json({
+        massage: "profile accessed",
+        authData
+      })
+    }
+  })
+})
+
+function verifyToken(req, resp, next){
+  const bearerHeader = req.headers['authorization'];
+  if(typeof bearerHeader !== 'undefined'){
+    const bearer = bearerHeader.split(" ");
+    const token = bearer[1];
+    req.token = token;
+    next();
+  } else {
+    resp.send({
+      result: 'Token is not valid'
+    })
+  }  
+}
 
 app.listen(5000, ()=> {
   console.log('app is running on 5000 port');
